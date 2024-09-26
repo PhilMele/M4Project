ceedb4ac0eb7661feb18cf270a6894673b02fee2
Phil | Doopee Doo <phil@doopeedoo.co.uk>
2024-09-26 15:31:26 +0100

    Add allauth + base templates

--- M4Project/M4Project/settings.py
+++ M4Project/M4Project/settings.py
@@ -93,6 +93,7 @@ SITE_ID = 1
 
 EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend'
 
+# Allauth settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
