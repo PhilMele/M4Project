3fb1e1e84b0f893e79e56e8765dae5bde6b88aeb
Phil | Doopee Doo <phil@doopeedoo.co.uk>
2024-10-08 11:38:59 +0100

    Add PostGres setup on local + add variables to .env

--- M4Project/M4Project/settings.py
+++ M4Project/M4Project/settings.py
@@ -126,10 +126,21 @@ WSGI_APPLICATION = 'M4Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'm4project',
+        'USER': 'postgres',
+        'PASSWORD': 'PostgresPassword1985!',
+        'HOST': 'localhost',
+        'PORT': '5432', 
     }
 }
 
