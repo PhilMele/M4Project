f398e4a2753757436e35294a0c556a282afab97e
Phil | Doopee Doo <phil@doopeedoo.co.uk>
2024-10-31 21:13:52 +0000

    Add `renderRatesTable()` to render rates in enter.html when `parking_id` is in params

--- static/js/parking_fee.js
+++ static/js/parking_fee.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const parkingIdFromParam = pathSplit[pathSplit.length -2]
     console.log(`parking id is ${parkingIdFromParam}`);
 
+    // Collect rates from `get_parking_rates()`
     function fetchRates(){
         if (parkingIdFromParam == "null") {
             parkingId = "null";
@@ -24,10 +25,30 @@ document.addEventListener('DOMContentLoaded', function(){
             .then(response => response.json())
             .then(data => {
                 console.log("Fetched rates:", data);
-             
+                renderRatesTable(data);
         })
+        .catch(error => console.error("theres an error when getting the rates", error))
         }
     }
+
+    function renderRatesTable(data){
+
+        // get the body of the table
+        const tbody = document.querySelector("table tbody")
+        tbody.inner = "";
+
+        // Adds data to table rwos
+        data.forEach(rate=>{
+            const tableRow = document.createElement('tr');
+            tableRow.innerHTML = `
+                <tb>${rate.rate_name}</tb>
+                <tb>${rate.hour_range}</tb>
+                <tb>${rate.rate}</tb>
+            `;
+            tbody.appendChild(tableRow);
+        });
+    }
+
     
         // if there is a parameter change Parking ID
         // if user has selected a parking name 
