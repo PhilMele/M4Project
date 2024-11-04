967186b47869f9e859ca39a00f9eecd4df86d98b
Phil | Doopee Doo <phil@doopeedoo.co.uk>
2024-11-04 21:20:44 +0000

    Add `parking_info()` + template

--- parking_management/urls.py
+++ parking_management/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('parking_manager_dashboard', views.parking_manager_dashboard, name='parking-manager-dashboard'),
+    path('parking_manager_dashboard/', views.parking_manager_dashboard, name='parking-manager-dashboard'),
+    path('parking_info/<int:parking_id>/', views.parking_info, name='parking-info'),
 
 ]
--- parking_management/views.py
+++ parking_management/views.py
@@ -3,10 +3,12 @@ from user_management.models import UserProfile
 from .models import Parking
 # Create your views here.
 def parking_manager_dashboard(request):
-    
-    all_parkings = Parking.objects.all()
     user_parking_list = Parking.objects.filter(user = request.user.userprofile)
-    print(f"all_parkings = {all_parkings}")
     print(f"user_parking_list = {user_parking_list}")
+    return render(request, 'dashboard/parking_manager_dashboard.html',{
+        'user_parking_list':user_parking_list,
+    })
 
-    return render(request, 'dashboard/parking_manager_dashboard.html',{})
+def parking_info(request, parking_id):
+    print(f"parking_info()")
+    return render(request, 'parking_info/parking_info.html',{})
