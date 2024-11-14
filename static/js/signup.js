b36545bc68ba0a8cb643465171ac4bbbff07001f
Phil | Doopee Doo <phil@doopeedoo.co.uk>
2024-11-14 19:32:57 +0000

    Add `emailField.addEventListener()` to signup.js to validate email format

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,1 +1,59 @@
 console.log("signup.js is active");
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // define form element
+    const form = document.querySelector('form'); 
+
+    // fields to validate
+    const emailField = form.querySelector('input[name="email"]');
+
+    // email check credits: https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
+    emailField.addEventListener('input', () => {
+        const email = emailField.value;
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        
+        hideError(emailField);
+        
+        if (!emailPattern.test(email)) {
+            displayError(emailField, "Email address is not valid.");
+        }
+    });
+
+    const genError = (message) => {
+        const error = document.createElement('div');
+        error.className = 'error-message';
+        error.style.color = 'red';
+        error.textContent = message;
+        return error;
+    };
+
+    function displayError(field, message) {
+        let error = field.nextElementSibling;
+        
+        if (!error || !error.classList.contains('error-message')) {
+            error = genError(message);
+            field.insertAdjacentElement('afterend', error);
+        } else {
+            error.textContent = message;
+        }
+        
+        field.classList.add('invalid');
+    }
+
+    function hideError(field) {
+        const error = field.nextElementSibling;
+        
+        if (error && error.classList.contains('error-message')) {
+            error.remove();
+        }
+        
+        field.classList.remove('invalid');
+    }
+
+    // email confirmation
+    // checkusername
+    // password
+    // password confirmation
+});
