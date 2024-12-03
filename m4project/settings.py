59efb25d42893aba49832ff5cc434970b1eb011c
Phil | Doopee Doo <phil@doopeedoo.co.uk>
2024-12-03 15:38:00 +0000

    Add to Readme : Password Reset Via Email section

--- m4project/settings.py
+++ m4project/settings.py
@@ -154,7 +154,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https' 
-DEFAULT_FROM_EMAIL = '<info.geopay@gmail.com>'
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
 # Allauth settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
