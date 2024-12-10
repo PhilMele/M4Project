8e8c75d47b53293d0ee76594d558155140e951d0
Phil | Doopee Doo <phil@doopeedoo.co.uk>
2024-12-10 14:34:56 +0000

    Edit admin.py in parking_activity app to meet indention standards

--- parking_activity/urls.py
+++ parking_activity/urls.py
@@ -6,11 +6,27 @@ urlpatterns = [
     path('enter/<int:parking_id>/', views.enter, name='enter_with_parking_id'),
     path('leave/<int:stay_id>/', views.leave, name='leave'),
     path('history/', views.history, name='history'),
-    path('get-parking-location/', views.get_parking_location, name='get_parking_location'),
-    path('get_parking_rates/<int:parking_id>/', views.get_parking_rates, name='get_parking_rates'),
+    path(
+        'get-parking-location/',
+        views.get_parking_location,
+        name='get_parking_location'
+        ),
+    path(
+        'get_parking_rates/<int:parking_id>/',
+        views.get_parking_rates,
+        name='get_parking_rates'),
 
-    #payment paths
-    path('payment_successful/', views.payment_successful, name='payment-successful'),
-    path('payment_cancelled/',views.payment_cancelled, name='payment-cancelled'),
-    path('stripe_webhook/', views.stripe_webhook, name='stripe_webhook'),
+    # payment paths
+    path(
+        'payment_successful/',
+        views.payment_successful,
+        name='payment-successful'),
+    path(
+        'payment_cancelled/',
+        views.payment_cancelled,
+        name='payment-cancelled'),
+    path(
+        'stripe_webhook/',
+        views.stripe_webhook,
+        name='stripe_webhook'),
 ]
