ececa9614bd54300b84b435e4a6387fb4330d1c7
Phil | Doopee Doo <phil@doopeedoo.co.uk>
2024-12-10 14:41:16 +0000

    Edit models.py in parking_activity app to meet indention standards

--- parking_activity/models.py
+++ parking_activity/models.py
@@ -6,12 +6,21 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 
-
 # Create your models here.
 class Stay(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True)
-    parking_name = models.ForeignKey(Parking, on_delete=models.SET_NULL, null=True, blank=True)
-    calculated_fee = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'))
+    user = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True, blank=True)
+    parking_name = models.ForeignKey(
+        Parking,
+        on_delete=models.SET_NULL,
+        null=True, blank=True)
+    calculated_fee = models.DecimalField(
+            max_digits=10,
+            decimal_places=2,
+            default=Decimal('0.00')
+        )
     stripe_checkout_id = models.CharField(max_length=500, default="")
     paid = models.BooleanField(default=False)
 
@@ -19,23 +28,50 @@ class Stay(models.Model):
         return f"{self.id}"
 
 
-#note: there is redundancy in some of the FK, but equally adds more visibility from admin panel
 class EnterParking(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True)
-    parking_name = models.ForeignKey(Parking, on_delete=models.SET_NULL, null=True, blank=True)
-    stay = models.ForeignKey(Stay, on_delete=models.SET_NULL, null=True, blank=True)
-    timestamp_enter = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    user = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True)
+    parking_name = models.ForeignKey(
+        Parking,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True)
+    stay = models.ForeignKey(
+        Stay,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True)
+    timestamp_enter = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+        blank=True)
 
     def __str__(self):
         return f"{self.id}"
 
 
 class LeaveParking(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True)
-    parking_name = models.ForeignKey(Parking, on_delete=models.SET_NULL, null=True, blank=True)
-    stay = models.ForeignKey(Stay, on_delete=models.SET_NULL, null=True, blank=True)
-    timestamp_leave = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    user = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True)
+    parking_name = models.ForeignKey(
+        Parking,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True)
+    stay = models.ForeignKey(
+        Stay,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True)
+    timestamp_leave = models.DateTimeField(
+        auto_now_add=True,
+        null=True, blank=True)
 
     def __str__(self):
         return f"{self.id}"
-
