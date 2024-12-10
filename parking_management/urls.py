1dd54e0f34b0f76af8cfe63a7fa02a53e472b01c
Phil | Doopee Doo <phil@doopeedoo.co.uk>
2024-12-10 15:24:44 +0000

    Edit urls.py in parking_management app to meet indention standards

--- parking_management/urls.py
+++ parking_management/urls.py
@@ -2,21 +2,52 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('parking_manager_dashboard/', views.parking_manager_dashboard, name='parking-manager-dashboard'),
-    path('parking_info/<int:parking_id>/', views.parking_info, name='parking-info'),
-    path('parking_inspector/<int:parking_id>/', views.parking_inspector, name='parking-inspector'),
-    path('delete_car_reg/<int:parking_id>/<int:illegalparking_id>/', views.delete_car_reg, name='delete-car-reg'),
+    path(
+        'parking_manager_dashboard/',
+        views.parking_manager_dashboard,
+        name='parking-manager-dashboard'),
+    path(
+        'parking_info/<int:parking_id>/',
+        views.parking_info,
+        name='parking-info'),
+    path(
+        'parking_inspector/<int:parking_id>/',
+        views.parking_inspector,
+        name='parking-inspector'),
+    path(
+        'delete_car_reg/<int:parking_id>/<int:illegalparking_id>/',
+        views.delete_car_reg,
+        name='delete-car-reg'),
 
     # parking objects
-    path('create_parking', views.create_parking, name='create-parking'),
-    path('edit_parking/<int:parking_id>/', views.edit_parking, name='edit-parking'),
-    path('delete_parking/<int:parking_id>/', views.delete_parking, name='delete-parking'),
-    path('activate_parking/<int:parking_id>/', views.activate_parking, name='activate-parking'),
-    
+    path(
+        'create_parking',
+        views.create_parking,
+        name='create-parking'),
+    path(
+        'edit_parking/<int:parking_id>/',
+        views.edit_parking,
+        name='edit-parking'),
+    path(
+        'delete_parking/<int:parking_id>/',
+        views.delete_parking,
+        name='delete-parking'),
+    path(
+        'activate_parking/<int:parking_id>/',
+        views.activate_parking,
+        name='activate-parking'),
+
     # rate objects
-    path('add_rate/<int:parking_id>/', views.add_rate, name='add-rate'),
-    path('edit_rate/<int:rate_id>/<int:parking_id>/', views.edit_rate, name='edit-rate'),
-    path('delete_rate/<int:rate_id>/<int:parking_id>/', views.delete_rate, name='delete-rate'),
-
-
+    path(
+        'add_rate/<int:parking_id>/',
+        views.add_rate,
+        name='add-rate'),
+    path(
+        'edit_rate/<int:rate_id>/<int:parking_id>/',
+        views.edit_rate,
+        name='edit-rate'),
+    path(
+        'delete_rate/<int:rate_id>/<int:parking_id>/',
+        views.delete_rate,
+        name='delete-rate'),
 ]
