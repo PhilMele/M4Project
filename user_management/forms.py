a9725cc99eebe975667aca8d1d4349f6640e3cb3
Phil | Doopee Doo <phil@doopeedoo.co.uk>
2024-12-10 16:30:30 +0000

    Edit forms.py in user_management app to meet indention standards

--- user_management/forms.py
+++ user_management/forms.py
@@ -4,6 +4,7 @@ from .models import UserProfile
 from django_countries.fields import CountryField
 from django_countries.widgets import CountrySelectWidget
 
+
 class UserProfileForm(ModelForm):
     class Meta:
         model = UserProfile
@@ -11,7 +12,7 @@ class UserProfileForm(ModelForm):
             'phone_number',
             'street_address1',
             'street_address2',
-            'city', 
+            'city',
             'county',
             'postcode',
             'country',
@@ -19,24 +20,48 @@ class UserProfileForm(ModelForm):
         )
 
         labels = {
-            'phone_number':'',
-            'street_address1':'',
-            'street_address2':'',
-            'city':'',
-            'county':'',
-            'postcode':'',
-            'country':'',
-            'car_registration':'',
+            'phone_number': '',
+            'street_address1': '',
+            'street_address2': '',
+            'city': '',
+            'county': '',
+            'postcode': '',
+            'country': '',
+            'car_registration': '',
         }
 
         widgets = {
-            'phone_number': forms.TextInput(attrs={'aria-label': 'User Phone Number', 'placeholder': 'Enter Phone Number'}),
-            'car_registration': forms.TextInput(attrs={'aria-label': 'User Car Registration Name', 'placeholder': 'Enter Car registration'}),
-            'street_address1': forms.TextInput(attrs={'aria-label': 'Street Address 1', 'placeholder': 'Enter street address 1'}),
-            'street_address2': forms.TextInput(attrs={'aria-label': 'Street Address 2', 'placeholder': 'Enter street address 2'}),
-            'city': forms.TextInput(attrs={'aria-label': 'City', 'placeholder': 'Enter city'}),
-            'county': forms.TextInput(attrs={'aria-label': 'County', 'placeholder': 'Enter county'}),
-            'postcode': forms.TextInput(attrs={'aria-label': 'Postcode', 'placeholder': 'Enter postcode'}),
-            'country': CountrySelectWidget(attrs={'aria-label': 'Country', 'class': 'w-100'}),
+            'phone_number': forms.TextInput(
+                attrs={
+                    'aria-label': 'User Phone Number',
+                    'placeholder': 'Enter Phone Number'}),
+            'car_registration': forms.TextInput(
+                attrs={
+                    'aria-label': 'User Car Registration Name',
+                    'placeholder': 'Enter Car registration'}),
+            'street_address1': forms.TextInput(
+                attrs={
+                    'aria-label': 'Street Address 1',
+                    'placeholder': 'Enter street address 1'}),
+            'street_address2': forms.TextInput(
+                attrs={
+                    'aria-label': 'Street Address 2',
+                    'placeholder': 'Enter street address 2'}),
+            'city': forms.TextInput(
+                attrs={
+                    'aria-label': 'City',
+                    'placeholder': 'Enter city'}),
+            'county': forms.TextInput(
+                attrs={
+                    'aria-label': 'County',
+                    'placeholder': 'Enter county'}),
+            'postcode': forms.TextInput(
+                attrs={
+                    'aria-label': 'Postcode',
+                    'placeholder': 'Enter postcode'}),
+            'country': CountrySelectWidget(
+                attrs={
+                    'aria-label': 'Country',
+                    'class': 'w-100'
+                }),
         }
-
